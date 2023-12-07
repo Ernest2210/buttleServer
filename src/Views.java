1d609b9217b44b0c7d77a9f461f6e7ddf5e03190
Ernest Farukshin <miniernest123@yandex.ru>
2023-12-07 03:37:35 +0300

    added createRoom view

--- src/Views.java
+++ src/Views.java
@@ -1,3 +1,10 @@
-public class Views {
+import java.util.UUID;
 
+public class Views {
+    public static void createRoom(Connection connection, String message) throws Exception {
+        UUID roomId = Server.createRoom(connection);
+
+        String answer = "waitSecondPlayer&room=" + roomId.toString() + "\n";
+        connection.sendMessage(answer);
+    }
 }
